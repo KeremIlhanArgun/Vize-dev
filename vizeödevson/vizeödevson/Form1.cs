ef0cd6f6036535b284fd1cb76f897c7b9baa8ccf
Kerem İlhan Argun <152810183+KeremIlhanArgun@users.noreply.github.com>
2023-12-03 22:19:22 +0300

    Oyun
    Doğru butonu bulma oyunu

---
+++ vizeödevson/vizeödevson/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using vizeödevson.Properties;
+
+namespace vizeödevson
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer sPlayer=new SoundPlayer(Resources.mixkit_video_game_bomb_alert_2803);
+        SoundPlayer sPla = new SoundPlayer(Resources.mixkit_video_game_win_2016);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+            string yazı = "Tebrikler doğru butonu buldunuz!!!!";
+
+            MessageBox.Show(yazı);
+            sPla.Play();
+        }
+
+        private void button34_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button38_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button37_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button40_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button36_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button39_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button35_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button26_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button27_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button29_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button41_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button48_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button47_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button30_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button28_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button31_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button42_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button49_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button32_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button46_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button43_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button44_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button53_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button52_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button45_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button51_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button50_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button54_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            sPlayer.Play();
+        }
+    }
+}
